6dcd62d9b3da24e9e45a8cfdf494f3f0f51ecef6
Milya Shakurova <shakurova@Milyas-MacBook-Air.local>
2021-12-01 02:20:33 +0300

    gh-pages added

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(
-    <BrowserRouter>
+    <BrowserRouter basename='/react-food'>
         <App />
     </BrowserRouter>,
     document.getElementById('root')
